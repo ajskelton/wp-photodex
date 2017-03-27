c5b3561abf2a49bdd67afceef21aaf0981efcf63
Anthony Skelton <anthony@Anthonys-MacBook-Pro.local>
2017-03-27 13:53:17 -0700

    Updated to version 1.0.3

--- bootstrap.php
+++ bootstrap.php
@@ -12,7 +12,7 @@
  * Plugin Name: WP Photodex
  * Plugin URI: https://anthonyskelton.com/wp-photodex
  * Description: A WordPress Plugin to share your pokemon photo collection
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: ajskelton
  * Author URI: https://anthonyskelton.com
  * Text Domain: wp-photodex
